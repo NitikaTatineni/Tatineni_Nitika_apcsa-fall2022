ee595ba7a48ccefd09b6b716db245c1363dcd8d4
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-18 11:14:34 -0700

    Unit 01

---
+++ Unit 01/src/StarsRunner.java
@@ -0,0 +1,44 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+import java.time.format.TextStyle;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes test = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwoBlankLines();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      test.printTwentyStars();
+      test.printTwentyDashes();
+      
+   }
+}
