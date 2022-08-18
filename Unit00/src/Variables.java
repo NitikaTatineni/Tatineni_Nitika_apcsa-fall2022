ce3e37e7ea1d68615c05da75cf6dddae44cc911c
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-18 10:40:30 -0700

    Update Variables.java

--- Unit00/src/Variables.java
+++ Unit00/src/Variables.java
@@ -18,24 +18,36 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
-
+		short shortOne = 120;
+		int intOne = 130;
+		long longOne = 110;
+		
 
 		//decimal variables
-
+		float floatOne = 23.32f;
+		double doubleOne = 99.57;
 
 		//other integer types
 
 
 		//other types
-
+		char charOne = 'A';
+		boolean booleanOne = true;
+		String StringOne = "Hello World";
+		
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
-		System.out.println("*                               *");
+		System.out.println("*Nitika Tatineni                               8/18/2022*");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - inOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*16 bit unsigned- charOne = "+charOne+"\t\t*");
 
 
 
