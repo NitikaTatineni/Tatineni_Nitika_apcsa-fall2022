e56645d4f49a87af34dba41f2ab3c551e932e700
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-18 10:13:47 -0700

    Unit 00

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,28 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Nitika
+//Date - 8/18/2022
+//Class - AP CSA
+//Lab  - Unit 00
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Nitika Tatineni \n\n" );
+		System.out.println("Rabbit!" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                 _    _ _          " );
+		System.out.println("               // \\_|0|0|            " );
+		System.out.println("             O//   \\(^w^)              " );
+		System.out.println("             [-v--v-]/             " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Nitika Tatineni \t  8/18/2022 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("NNNNNNNNNNNNNNNNNNNNNNNNN " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("NNNNNNNNNNNNNNNNNNNNNNNNN " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("NNNNNNNNNNNNNNNNNNNNNNNNN " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("NNNNNNNNNNNNNNNNNNNNNNNNN " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+	}
+}
