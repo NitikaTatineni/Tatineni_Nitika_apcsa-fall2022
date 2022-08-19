09e46c0b1a360b992b93a8a39dd0d35845c2a9b6
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-19 11:07:22 -0700

    Unit 02 Update

--- Unit 02/src/Line.java
+++ Unit 02/src/Line.java
@@ -1,13 +1,13 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Nitika Tatineni
+//Date - 08/19/2022
 
 public class Line
 {
 	public static double getSlope( int x1, int y1, int x2, int y2 )
 	{
-		return 0;
+		return (double)(y2-y1) / (double)(x2-x1);
 	}
 
 }
--- Unit 02/src/LineRunner.java
+++ Unit 02/src/LineRunner.java
@@ -1,8 +1,8 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  - 
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
+//Name - Nitika Tatineni
+//Date - 08/19/2022
+//Class - AP CSA
+//Lab  - Line Runner
 
 public class LineRunner
 {
