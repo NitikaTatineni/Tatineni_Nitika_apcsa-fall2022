a0eb0fdea69d9c411225d7e5c55d047959aecfb4
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-19 10:34:14 -0700

    Unit 02

---
+++ Unit 02/src/Trap.java
@@ -0,0 +1,13 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Nitika Tatineni
+//Date - 08/19/2022
+//Class - AP CSA
+//Lab  - Trap
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return (0.5)*(base1 + base2)*(height);
+	}
+}
