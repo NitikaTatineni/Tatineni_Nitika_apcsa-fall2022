969eeee5ec491585b37a90622d21f9bfab6b92d2
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-23 10:23:17 -0700

    Unit 05

---
+++ Unit 05/src/AvgLen.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Nitika Tatineni
+//Date - 08/23/2022
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+		return (double) (0.5)*((a.length())+(b.length()));
+	}
+}
