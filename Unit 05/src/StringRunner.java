9b472e271c8eef01583d7fa3a04530f1a8e2173b
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-23 11:49:16 -0700

    Update StringRunner.java

--- Unit 05/src/StringRunner.java
+++ Unit 05/src/StringRunner.java
@@ -13,14 +13,47 @@ public class StringRunner
 {
 	public static void main ( String[] args )
 	{
-		Scanner keyboard = new Scanner(System.in);
-		out.print("Enter a word :: ");
 		
-		String word = keyboard.next();
+		StringOddOrEven s = new StringOddOrEven();
 		
-		StringOddOrEven test = new StringOddOrEven(word);
+		System.out.println("Enter a word :: cat");
+		s.setString("cat");
+		s.isEven();
+		System.out.println(s.toString()) ;
 		
-		test.isEven();
-		out.println(test.toString());
+		System.out.println("Enter a word :: boot");
+		s.setString("boot");
+		s.isEven();
+		System.out.println(s.toString()) ;
+		
+		System.out.println("Enter a word :: it");
+		s.setString("it");
+		s.isEven();
+		System.out.println(s.toString()) ;
+		
+		System.out.println("Enter a word :: a");
+		s.setString("a");
+		s.isEven();
+		System.out.println(s.toString()) ;
+		
+		System.out.println("Enter a word :: eleven");
+		s.setString("eleven");
+		s.isEven();
+		System.out.println(s.toString()) ;
+		
+		System.out.println("Enter a word :: thirteen");
+		s.setString("thirteen");
+		s.isEven();
+		System.out.println(s.toString()) ;
+		
+		System.out.println("Enter a word :: odd");
+		s.setString("odd");
+		s.isEven();
+		System.out.println(s.toString()) ;
+		
+		System.out.println("Enter a word :: even");
+		s.setString("even");
+		s.isEven();
+		System.out.println(s.toString()) ;
 	}
 }
