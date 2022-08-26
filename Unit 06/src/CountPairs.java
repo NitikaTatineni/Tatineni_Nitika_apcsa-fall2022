dd1b3a8faf02faa6a4d6a6121230939fe17094de
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-26 10:34:27 -0700

    Unit 06

--- Unit 06/src/CountPairs.java
+++ Unit 06/src/CountPairs.java
@@ -9,6 +9,12 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
+		int n = str.length();
+		for (int i=0; i < n-1; i=i+1) {
+			if (str.charAt(i)==(str.charAt(i+1))) {
+				count = count + 1;
+			}
+		}
 		return count;
 	}
 }
