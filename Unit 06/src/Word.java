fe5a5120872f42d3b16fdc53e1d89382ffc689bd
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-26 11:32:21 -0700

    Unit 06 Update

--- Unit 06/src/Word.java
+++ Unit 06/src/Word.java
@@ -1,6 +1,6 @@
-//(c) A+ Computer Science
+//(c) A+ Computer Sciencennnnn
 //www.apluscompsci.com
-//Name
+//Name: Nitika Tatineni
 
 import static java.lang.System.*;
 
@@ -10,34 +10,44 @@ public class Word
 
 	public Word()
 	{
+		word = "";
 	}
 
 	public Word(String s)
 	{
+		word = s;
 	}
 
 	public void setString(String s)
 	{
+		word = s;
 	}
 
 	public char getFirstChar()
 	{
-		return 0;
+		return word.charAt(0);
 	}
 
 	public char getLastChar()
 	{
-		return 0;
+		return word.charAt(word.length()-1);
 	}
 
 	public String getBackWards()
 	{
 		String back="";
+		int n = word.length();
+		String cutword = word;
+		while (n > 0) {
+			back += cutword.charAt(n-1);
+			cutword = cutword.substring(0,n-1);
+			n--;
+		}
 		return back;
 	}
 
  	public String toString()
  	{
- 		return "";
+ 		return getFirstChar() + "\n" + getLastChar() + "\n" + getBackWards() + "\n" + word + "\n";
 	}
 }
--- Unit 06/src/WordRunner.java
+++ Unit 06/src/WordRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name: Nitika Tatineni
 
 import static java.lang.System.*;
 
@@ -8,6 +8,21 @@ public class WordRunner
 {
 	public static void main ( String[] args )
 	{
+		Word s = new Word();
+		s.setString("Hello");
+		System.out.println(s);
+		
+		s.setString("World");
+		System.out.println(s);
+		
+		s.setString("JukeBox");
+		System.out.println(s);
+		
+		s.setString("TCEA");
+		System.out.println(s);
+		
+		s.setString("UIL");
+		System.out.println(s);
 		//add test cases		
 	}
 }
