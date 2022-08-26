310165d5738009c407311aedc043d43f8f339f62
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-08-26 16:20:11 -0700

    Unit 06 Update

--- Unit 06/src/Perfect.java
+++ Unit 06/src/Perfect.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Nitika Tatineni
 
 import static java.lang.System.*;
 
@@ -9,14 +9,46 @@ public class Perfect
    private int number;
 
 	//add constructors
+   public Perfect() {
+	   number = 0;
+   }
+   
+   public Perfect(int n) {
+	   number = n;
+   }
 
 	//add a set method
-
-	public boolean isPerfect()
-	{
-		return false;
+   public void setNum(int n) {
+	   number = n;
+   }
+   
+   public boolean isPerfect()
+   {
+	   int num = 1;
+	   int div = 0;
+	   while (number > num) {
+		   if (number % num == 0) {
+			   div = div + num;
+		   }
+		   num = num + 1;
+		}
+		
+	   if ( div == number ) {
+			return true;
+		}
+	   else {
+		   return false;
+	   }
 	}
 
 	//add a toString	
+	public String toString() {
+		if (isPerfect()) {
+			return "" + number + " is perfect.";
+		}
+		else { 
+			return "" + number + " is not perfect.";
+		}
+	}
 	
 }
--- Unit 06/src/PerfectRunner.java
+++ Unit 06/src/PerfectRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Nitika Tatineni
 
 import static java.lang.System.*; 
 
@@ -9,6 +9,35 @@ public class PerfectRunner
 	public static void main( String args[] )
 	{
 		//add test cases
-																
+		Perfect s = new Perfect();
+		s.setNum(496);
+		System.out.println(s);
+		
+		s.setNum(45);
+		System.out.println(s);
+		
+		s.setNum(6);
+		System.out.println(s);
+		
+		s.setNum(14);
+		System.out.println(s);
+		
+		s.setNum(8128);
+		System.out.println(s);
+		
+		s.setNum(1245);
+		System.out.println(s);
+		
+		s.setNum(33);
+		System.out.println(s);
+		
+		s.setNum(28);
+		System.out.println(s);
+		
+		s.setNum(27);
+		System.out.println(s);
+		
+		s.setNum(33550336);
+		System.out.println(s);
 	}
 }
