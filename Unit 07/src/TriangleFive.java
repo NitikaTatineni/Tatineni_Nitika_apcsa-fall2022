340263b64f1a125f93fa5807daa6f844b614862a
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-09-08 16:51:46 -0700

    Unit 07 update

--- Unit 07/src/TriangleFive.java
+++ Unit 07/src/TriangleFive.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Nitika Tatineni
 
 import static java.lang.System.*;
 
@@ -11,23 +11,52 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		letter = ' ';
+		amount = 0;
+		setLetter(' ');
+		setAmount(0);
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		letter = c;
+		amount = amt;
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
 		String output="";
-		return output;
+		for(int i = 0; i < amount; i++) {
+			output += "\n";
+			int cnt = amount + 1;
+			for(char j = letter; j < amount + letter - i; j++) { 
+				cnt--;
+				for(int k = cnt; k > 0; k--) {
+					char c1 = j;
+					if(c1 > 90) {
+						c1 = (char) (c1-26);
+					}
+						if(k == 1) {
+							output += c1 + " ";
+						}
+						else {
+							output += c1;
+						}
+				}
+			}
+		}
+		return output ;
 	}
 }
--- Unit 07/src/TriangleFiveRunner.java
+++ Unit 07/src/TriangleFiveRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Nitika Tatineni
 
 import static java.lang.System.*;
 
@@ -8,5 +8,25 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
+	   TriangleFive s = new TriangleFive();
+	   s.setLetter('C');
+	   s.setAmount(4);
+	   System.out.println(s);
+	   
+	   s.setLetter('A');
+	   s.setAmount(5);
+	   System.out.println(s);
+	   
+	   s.setLetter('B');
+	   s.setAmount(7);
+	   System.out.println(s);
+	   
+	   s.setLetter('X');
+	   s.setAmount(6);
+	   System.out.println(s);
+	   
+	   s.setLetter('Z');
+	   s.setAmount(8);
+	   System.out.println(s);
 	}
 }
