6b63f8a55183a4fb0981a946af77ed94c5da29aa
NitikaTatineni <95978722+NitikaTatineni@users.noreply.github.com>
2022-09-09 11:11:14 -0700

    Unit 09 Update

--- Unit 09/src/CardTester.java
+++ Unit 09/src/CardTester.java
@@ -9,5 +9,16 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card one = new Card("9", "Spades", 9);
+		Card two = new Card("3", "Hearts", 3);
+		Card three = new Card ("Queen", "Diamonds", 12);
+		
+		System.out.println("Card One: " + one);
+		System.out.println("Card One: " + two);
+		System.out.println("Card One: " + three);
+		
+		System.out.println("Card One matches Card Two: " + one.matches(two));
+		System.out.println("Card Two matches Card Three: " + two.matches(three));
+		System.out.println("Card Three matches Card One: " + three.matches(one));
 	}
 }
